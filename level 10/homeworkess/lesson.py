288dfb3c47dbe17f7e2f5bfbd40155848411997f
GiorgiMaxata <gio4makhatadze@gmail.com>
2024-06-13 15:02:41 +0400

    commit

---
+++ level 10/homeworkess/lesson.py
@@ -0,0 +1,32 @@
+for i in range(0, 30 + 1 , 2 ):
+    print(i)
+
+for i in range(20 ,0, - 2):
+     print(i)
+
+for G in range(1, 67 + 1 , 2 ):
+    print(G)
+
+for i in range(0, 7 + 1 , 2 ):
+    print("lupa")
+
+for i in range(0, 3 + 1 , 2 ):
+    print("enter your pasword")
+
+for i in range(0, 2 + 1 , 3 ):
+    print("enter your name")
+
+
+for i in range(0, 1 + 1 , 1 ):
+    print("re enter  your name ")
+ 
+for i in range(0, 2 + 1 , 3 ):
+    print("re enter  your pasword ")
+    
+for i in range(0, 2 + 1 , 3 ):
+    print("re enter  your nick name ")
+
+for i in range(0, 2 + 1 , 3 ):
+    print("re enter  your phoner number ")
+
+
