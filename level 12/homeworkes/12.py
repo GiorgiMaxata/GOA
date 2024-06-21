612cf0c85054ce6382c7d0461c4e5891da0b9760
GiorgiMaxata <gio4makhatadze@gmail.com>
2024-06-21 17:53:32 +0400

    commit

---
+++ level 12/homeworkes/12.py
@@ -0,0 +1,35 @@
+#for i in range(1, 11):
+    #print(i)
+ 
+#for i in range(5):
+    #print("Hello")
+
+#i=1
+#while i <= 5 :
+    #i += 1
+    #print("hello") 
+
+
+    #1
+#"""user = int(input("enter your age: "))
+
+#if user > 0:
+    #print("user age dadebitia") 
+#else:
+    #print("user age uayropitia ")"""
+
+
+#2
+#"""age = int(input("enter your age: "))
+
+#if age == 10:
+    #print("user age equals 10")"""
+
+
+#3
+#"""age  = int(input("enter your age: "))
+
+#if age > 0 and age < 10:
+    #print("user is a child")
+#else:
+    #print("user isnt child")"""
